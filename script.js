2718a83851ce7ade1c8b9d4a6b108a0c276212a0
Ellis <222089829+Ellis-P@users.noreply.github.com>
2025-08-10 19:46:51 +0200

    Add function that creates and appends 16 tiles to the grid container

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ const gridContainer = document.querySelector(".grid-container");
 
 function createTile() {
   const tile = document.createElement("div");
-  tile.className("tile");
+  tile.className = "tile";
+  return tile;
+}
+
+function generateGrid() {
+  for (let i = 0; i < 16; i++) {
+    const tile = createTile();
+    gridContainer.appendChild(tile);
+  }
 }
