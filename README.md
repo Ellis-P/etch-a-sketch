09a8a8fe3a5093ba0253a357d8b9e7cb2a1069c3
Ellis <222089829+Ellis-P@users.noreply.github.com>
2025-08-10 18:58:11 +0200

    Add function to create a single tile

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # etch-a-sketch
+
+- Create a function to create a single square
+- Use a loop to create 16 squares and append them to the grid-container
+- 
---
+++ script.js
@@ -0,0 +1,6 @@
+const gridContainer = document.querySelector(".grid-container");
+
+function createTile() {
+  const tile = document.createElement("div");
+  tile.className("tile");
+}
